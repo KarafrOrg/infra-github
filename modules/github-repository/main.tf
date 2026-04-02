e62cdd9e47acb0083d8570044669c2271e9d4461
matustoth <55988328+karafra@users.noreply.github.com>
2026-04-02 22:10:16 +0200

    Add lock

--- modules/github-repository/main.tf
+++ modules/github-repository/main.tf
@@ -103,9 +103,9 @@ resource "github_repository_ruleset" "ruleset" {
       }
     }
 
-    required_signatures = try(each.value.require_signed_commits, false)
-    required_linear_history = try(each.value.required_linear_history, false)
-    non_fast_forward = try(each.value.allow_force_pushes, false) ? false : true
+    required_signatures = coalesce(try(each.value.require_signed_commits, null), false)
+    required_linear_history = coalesce(try(each.value.required_linear_history, null), false)
+    non_fast_forward = coalesce(try(each.value.allow_force_pushes, null), false)
     deletion = !try(each.value.allow_deletions, false)
   }
 }
